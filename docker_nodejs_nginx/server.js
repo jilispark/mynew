a388b9434a39b88a7a920f8dcf531cf1c5cf744b
Prince Joseph <princejoseph4043@gmail.com>
2021-03-10 15:37:19 +0530

    joseph

--- docker_nodejs_nginx/server.js
+++ docker_nodejs_nginx/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Good morning all');
+  res.send('Good morning joseph');
 });
 
 app.listen(PORT, HOST);
